6abf92ee7bb02ba8eec95b1df9663c5e21f7014f
Obidjon91 <obidjon961@gmail.com>
2025-02-19 08:30:50 +0300

    frist commit

---
+++ CalcBarcode/src/Modules.py
@@ -0,0 +1,35 @@
+import flet as ft
+
+
+class BtnMenu(ft.IconButton):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.page = page
+        # self.icon = ft.icons.MENU_OPEN
+        self.icon = ft.icons.MENU
+
+
+
+class Header(ft.Container):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.page = page
+        self.bgcolor = "#C0C0C0"
+        self.content = ft.Row(
+            controls=[
+                BtnMenu(page),
+            ]
+        )
+
+
+class ConBtn(ft.Container):
+    def __init__(self, page: ft.Page, func_on_click = None):
+        super().__init__()
+        self.page = page
+        self.on_click = func_on_click
+        self.bgcolor = "#C0C0C0"
+        self.content = ft.Row(
+            controls=[
+                ft.Text(value="Заказ")
+            ]
+        )
---
+++ CalcBarcode/src/main.py
@@ -0,0 +1,22 @@
+import flet as ft
+
+from Modules import Header, ConBtn
+
+def main(page: ft.Page):
+    page.title = "CalcBarcode"
+    page.horizontal_alignment = "start"
+    page.vertical_alignment = "start"
+    page.window.min_height = 600
+    page.window.min_width = 350
+    page.window.max_height = 600
+    page.window.max_width = 350
+    page.bgcolor = "#F2F3F4"
+    page.add(
+        Header(page=page),
+        ConBtn(page=page),
+    )
+    page.update()
+
+
+if __name__ == "__main__":
+    ft.app(main)
